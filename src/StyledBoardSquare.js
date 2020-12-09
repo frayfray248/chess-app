ee640966fbab4babb0086213118fbff489f73590
frayfray248 <nicman248@hotmail.com>
2020-12-08 16:14:01 -0800

    centered each StyledBoardSquare's text

--- src/StyledBoardSquare.js
+++ src/StyledBoardSquare.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledBoardSquare = styled.td`
     width: 2em;
     height: 2em;
+    text-align: center;
     background-color: ${props => props.dark ? "#6E6E6E" : "#F3F3F3"}
 `;
 

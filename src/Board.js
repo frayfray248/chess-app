62fc8269b0769299dfe4d8f95c4de9545c81baf2
frayfray248 <nicman248@hotmail.com>
2020-12-08 16:14:54 -0800

    Automated board 2D array creation

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,27 @@
 import StyledBoardSquare from "./StyledBoardSquare";
 
+// board dimensions
+const boardWidth = 8;
+const boardHeight = 8;
+
+// constructing board IIFE
+const boardArr = (() => {
+    // The code below creates a 2D array using spreading and mapping.
+    // The elements are alternating booleans to determine if a chess 
+    // square is dark or light.
+    // Each row is filled with alternating booleans
+    // Each row also alternates starting booleans
+    return [...new Array(boardHeight)].map((_, colIndex) => 
+    [... new Array(boardWidth)].map((_, rowIndex) => {
+        if (colIndex % 2 === 0) {
+            return rowIndex % 2 !== 0
+        } else {
+            return rowIndex % 2 === 0
+        }
+    }))
+})();
+
+
 const Board = () => (
     <table>
         <tbody>
@@ -14,94 +36,12 @@ const Board = () => (
                 <th>g</th>
                 <th>h</th>
             </tr>
-            <tr>
-                <th>8</th>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>7</th>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>6</th>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>5</th>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>4</th>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>3</th>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>2</th>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-            </tr>
-            <tr>
-                <th>1</th>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-                <StyledBoardSquare dark={true} ></StyledBoardSquare>
-                <StyledBoardSquare dark={false} ></StyledBoardSquare>
-            </tr>
+            {boardArr.map((row, index) => (
+                <tr>
+                    <th>{index}</th>
+                    {row.map(dark => <StyledBoardSquare dark={dark}/>)}
+                </tr>
+            ))}
         </tbody>
     </table>
 );
